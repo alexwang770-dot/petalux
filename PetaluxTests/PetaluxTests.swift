f6c68c1771c7536d18f2da10d1b5cc9c30d9a82c
Alexander Wang <alexander.wang@salesianum.org>
2026-04-20 19:26:32 -0700

    first commit, empty project

---
+++ PetaluxTests/PetaluxTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetaluxTests.swift
+//  PetaluxTests
+//
+//  Created by Alexander Wang on 4/20/26.
+//
+
+import Testing
+@testable import Petalux
+
+struct PetaluxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
