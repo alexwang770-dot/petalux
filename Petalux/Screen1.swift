77ead15073adc1e49a41c90abe70cdb33d4eadcf
naktheking <78290773+naktheking@users.noreply.github.com>
2026-04-22 17:40:43 -0700

    got rid of unnecessary files and testing files

--- Petalux/Screen1.swift
+++ Petalux/Screen1.swift
@@ -5,3 +5,15 @@
 //  Created by Alexander Wang on 4/22/26.
 //
 
+import SwiftUI
+
+struct SecondPageView: View {
+    var body: some View {
+        Text("Hello from screen 2")
+    }
+}
+
+
+#Preview {
+    SecondPageView()
+}
--- Petalux/Screen2.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Screen2.swift
-//  Petalux
-//
-//  Created by Alexander Wang on 4/22/26.
-//
-
